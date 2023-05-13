6e04a6633f052f041a6e226a4f752843b47e629b
Lucas Gauna <lucasDeveloper98@gmail.com>
2023-05-13 13:07:06 -0300

    Interces terminadas(login,register,pages)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PagesComponent } from './pages/pages.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   //Main Routes
+     {
+      path:'login', component:LoginComponent
+     },
+     {
+      path:'register', component: RegisterComponent
+     },
+      //Childres Routes
+      {
+        //Protected Routes
+        path:'',
+        component: PagesComponent,
+        children:[
+          {
+            path:'', pathMatch:'full', redirectTo:'/dashboard'
+          },
+          {
+            path:'dashboard', component:DashboardComponent
+          },
+          {
+            path:'grafica1', component:Grafica1Component
+          },
+          {
+            path:'progress', component:ProgressComponent
+          },
+          {
+            path:'**', component:PageNotFoundComponent
+          },
+        ],
+      },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    DashboardComponent,
+    PageNotFoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    BreadcrumbsComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
